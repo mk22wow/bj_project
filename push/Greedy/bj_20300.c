37ee08828c2fe395d8380e7671d451e15c526c9c
mk22 <tiger2962@gmail.com>
2026-01-10 03:10:41 +0900

    bj_20300

---
+++ push/Greedy/bj_20300.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int N;
+long long list[10000];
+long long M = LLONG_MIN;
+
+int Compare(const void* a, const void* b){
+    if(*(long long*)a<*(long long*)b){
+        return -1;
+    }
+    else return 1;
+}
+
+int main(void){
+    scanf("%d", &N);
+    for(int i=0; i<N; i++){
+        scanf("%lld", &list[i]);
+    }
+    
+
+    qsort(list, N, sizeof(long long), Compare);
+    for(int i=0; i<N; i++){
+        printf("%lld ", list[i]);
+    }
+
+    if(N%2 != 0){
+        M = list[--N];
+    }
+
+    for(int i=0; i<N/2; i++){
+        long long temp = list[i] + list[N-1-i];
+        if(temp>M) M = temp;
+    }
+
+    printf("%lld\n", M);
+    return 0;
+}
