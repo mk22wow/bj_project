bd62949603320e3b7ed47b739cca419a2a4a2bb2
mk22 <tiger2962@gmail.com>
2026-01-28 18:45:18 +0900

    bj_34830

--- push/ICPC_26W_2/bj_34830.c
+++ push/ICPC_26W_2/bj_34830.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
-int result[200002];
-
 int main(void){
-    int N;
-    int count = 0;
-    if(scanf("%d", &N) != 1) return 0;
+    long long N;
+    long long count = 0;
+    if(scanf("%lld", &N) != 1) return 0;
     if(N%2 != 0){
         count = N*(N-1) / 2;
-        printf("%d\n", count);
+        printf("%lld\n", count);
     }
     else{
         count = N*(N-1) / 2;
         count += (N-2) / 2;
-        printf("%d\n", count); 
+        printf("%lld\n", count); 
     }
     return 0;
 }
