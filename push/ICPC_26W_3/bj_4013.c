0cfcb65bb6797ec25fcdc0d63580359b0db15e42
mk22 <tiger2962@gmail.com>
2026-01-30 02:48:12 +0900

    bj_4013

--- push/ICPC_26W_3/bj_4013.c
+++ push/ICPC_26W_3/bj_4013.c
@@ -140,3 +140,133 @@ int main(void){
     printf("%d\n", answer);
     return 0;
 }
+
+// c++ 예시 코드
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// typedef long long ll;
+
+// int disc[500000];
+// int low[500000];
+// bool on_stack[500000];
+// stack<int> st;
+// vector<vector<int>> g;
+// vector<vector<int>> sccs;
+// int t = -1;
+
+// ll node_cash[500000];
+// bool final_state[500000];
+// int scc_id[500000];
+// ll scc_cash[500000];
+// bool scc_has_fin[500000];
+// ll dp[500000];
+
+// void getSCC(int cur) {
+//     disc[cur] = low[cur] = ++t;
+//     st.push(cur);
+//     on_stack[cur] = true;
+
+//     for (int nxt : g[cur]) {
+//         if (disc[nxt] == -1) {
+//             getSCC(nxt);
+//             low[cur] = min(low[cur], low[nxt]);
+//         } else if (on_stack[nxt]) {
+//             low[cur] = min(low[cur], disc[nxt]);
+//         }
+//     }
+
+//     if (low[cur] == disc[cur]) {
+//         vector<int> current_scc;
+//         while (true) {
+//             int v = st.top(); st.pop();
+//             on_stack[v] = false;
+//             current_scc.push_back(v);
+//             if (cur == v) break;
+//         }
+//         sort(current_scc.begin(), current_scc.end());
+//         sccs.push_back(current_scc);
+//     }
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     // 입력
+//     int N, M; cin >> N >> M;
+//     g.resize(N);
+//     memset(disc, -1, sizeof(disc));
+//     for (int i = 0; i < M; i++) {
+//         int u, v; cin >> u >> v;
+//         g[u - 1].push_back(v - 1);
+//     }
+//     for (int i = 0; i < N; i++) {
+//         cin >> node_cash[i];
+//     }
+//     int S, P;
+//     cin >> S >> P;
+//     S--;
+//     memset(final_state, 0, sizeof(final_state));
+//     for (int i = 0; i < P; i++) {
+//         int x; cin >> x;
+//         final_state[x - 1] = true;
+//     }
+    
+//     // SCC 알고리즘 돌려서 SCC 구하기
+//     for (int i = 0; i < N; i++) {
+//         if (disc[i] == -1) getSCC(i);
+//     }
+    
+//     // 위상정렬 순서로 만들기 위해 뒤집음
+//     reverse(sccs.begin(), sccs.end());
+    
+//     // 정점 a는 b번 SCC에 속한다는 mapping을 만듦
+//     memset(scc_id, -1, sizeof(scc_id));
+//     for (int i = 0; i < sccs.size(); i++) {
+//         for (int v : sccs[i]) {
+//             scc_id[v] = i;
+//         }
+//     }
+    
+//     // SCC별로 묶는 작업 (돈 총량, 종료 정점 있는지)
+//     for (int i = 0; i < sccs.size(); i++) {
+//         scc_cash[i] = 0;
+//         scc_has_fin[i] = false;
+//         for (int v : sccs[i]) {
+//             scc_cash[i] += node_cash[v];
+//             if (final_state[v]) {
+//                 scc_has_fin[i] = true;
+//             }
+//         }
+//     }
+    
+//     // DP
+//     // dp[i] = -1 : i번 SCC가 도달 불가
+//     for (int i = 0; i < sccs.size(); i++) dp[i] = -1;
+//     int start_id = scc_id[S];
+//     dp[start_id] = scc_cash[start_id];
+//     for (int i = 0; i < sccs.size(); i++) {
+//         if (dp[i] == -1) continue;
+//         // 직접 새로운 그래프에 해당하는 2차원 벡터를 만들지는 않음
+//         // 그 대신 SCC 안에 들어있는 정점에서 나가는 간선들을 확인
+//         for (int u : sccs[i]) {
+//             for (int v : g[u]) {
+//                 int next_id = scc_id[v];
+//                 if (i != next_id) {
+//                     dp[next_id] = max(dp[next_id], dp[i] + scc_cash[next_id]);
+//                 }
+//             }
+//         }
+//     }
+    
+//     // 종료 정점이 있는 SCC만 생각해 정답 출력
+//     ll ans = 0;
+//     for (int i = 0; i < sccs.size(); i++) {
+//         if (scc_has_fin[i]) {
+//             ans = max(ans, dp[i]);
+//         }
+//     }
+//     cout << ans << "\n";
+//     return 0;
+// }
