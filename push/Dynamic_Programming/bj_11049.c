570c79a273067dc19905cd104ec57d3af048630d
mk22 <tiger2962@gmail.com>
2026-02-11 20:16:45 +0900

    bj_11049

--- push/Dynamic_Programming/bj_11049.c
+++ push/Dynamic_Programming/bj_11049.c
@@ -16,19 +16,18 @@ int main(void){
         scanf("%d %d", &list[i].a, &list[i].b);
     }
 
-    memset(DP, -1, sizeof(DP));
     for(int i=1; i<=N; i++){
         DP[i][i] = 0;
     }
 
     for(int i=1; i<N; i++){
         for(int j=1; j<=N-i; j++){
+            DP[j][j+i] = INT_MAX;
             int Min = INT_MAX;
             for(int k=j; k<j+i; k++){
                 int temp = DP[j][k] + DP[k+1][j+i] + list[j].a*list[k].b*list[j+i].b;
-                if(Min>temp) Min = temp;
+                if(DP[j][j+i]>temp) DP[j][j+i] = temp;
             }
-            DP[j][j+i] = Min;
         }
     }
 
